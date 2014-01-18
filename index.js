ef0a167ecc86a5a13a49d9e1d823e816a6958c8b
Mikael Brevik <mikaelbre@gmail.com>
2014-01-18 16:59:06 +0100

    Updates to use through2 as dependancy instead of map-stream.

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 'use strict';
 var crypto = require('crypto');
 var path = require('path');
-var map = require('map-stream');
+var through = require('through2');
 
 function md5(str) {
 	return crypto.createHash('md5').update(str, 'utf8').digest('hex');
 }
 
 module.exports = function (data, options) {
-	return map(function (file, cb) {
+	return through.obj(function (file, enc, cb) {
 		var hash = md5(file.contents.toString()).slice(0, 8);
 		var ext = path.extname(file.path);
 		var filename = path.basename(file.path, ext) + '-' + hash + ext;
 		file.path = path.join(path.dirname(file.path), filename);
-		cb(null, file);
+		this.push(file);
+		cb();
 	});
 };
