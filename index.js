a6bb519c240d8acf30d4c0255d423b3e2c6e11dc
Mikael Brevik <mikaelbre@gmail.com>
2014-01-18 17:07:37 +0100

    Adds check if content exists and if content is stream.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 var crypto = require('crypto');
 var path = require('path');
 var through = require('through2');
+var gutil = require('gulp-util');
 
 function md5(str) {
 	return crypto.createHash('md5').update(str, 'utf8').digest('hex');
@@ -9,6 +10,14 @@ function md5(str) {
 
 module.exports = function (data, options) {
 	return through.obj(function (file, enc, cb) {
+		if (file.isNull()) {
+			this.push(file);
+			return cb();
+		}
+		if (file.isStream()) {
+			this.emit('error', new gutil.PluginError('gulp-rev', 'Stream content is not supported'));
+			return callback();
+		}
 		var hash = md5(file.contents.toString()).slice(0, 8);
 		var ext = path.extname(file.path);
 		var filename = path.basename(file.path, ext) + '-' + hash + ext;
