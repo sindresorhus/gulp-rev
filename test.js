3f8834ab1a763ac89b1bd2ee7bb26f9a1b4ac8d9
Justin Hileman <justin@justinhileman.info>
2014-03-29 18:13:43 -0700

    Add a failing test for #16

--- test.js
+++ test.js
@@ -40,3 +40,49 @@ it('should build a rev manifest file', function (cb) {
 	stream.write(file);
 	stream.end();
 });
+
+
+it('should respect directories', function (cb) {
+	var stream = rev.manifest();
+
+	stream.on('data', function (newFile) {
+		assert.equal(newFile.relative, 'rev-manifest.json');
+		assert.deepEqual(
+			JSON.parse(newFile.contents.toString()),
+			{
+				'foo/unicorn.css': 'foo/unicorn-d41d8cd9.css',
+				'bar/pony.css':    'bar/pony-d41d8cd9.css'
+			}
+		);
+		cb();
+	});
+
+	var file1 = new gutil.File({
+		cwd:  __dirname,
+		base: __dirname,
+		path: __dirname + '/foo/unicorn-d41d8cd9.css',
+		contents: new Buffer('')
+	});
+
+	file1.revOrigBase = __dirname;
+	file1.revOrigPath = __dirname + '/foo/unicorn.css';
+	file1.origName    = 'unicorn.css';
+	file1.revName     = 'unicorn-d41d8cd9.css';
+
+	var file2 = new gutil.File({
+		cwd:  __dirname,
+		base: __dirname,
+		path: __dirname + '/bar/pony-d41d8cd9.css',
+		contents: new Buffer('')
+	});
+
+	file2.revOrigBase = __dirname;
+	file2.revOrigPath = __dirname + '/bar/pony.css';
+	file2.origName    = 'pony.css';
+	file2.revName     = 'pony-d41d8cd9.css';
+
+	stream.write(file1);
+	stream.write(file2);
+	stream.end();
+});
+
