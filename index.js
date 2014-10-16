6aaedc69f94d0cefe4c6053b2732deb81f1ec587
Martin Mårtensson <Marwelln@gmail.com>
2014-10-16 15:02:29 +0200

    Close #67 PR: All OS's should use the same path separator.

--- index.js
+++ index.js
@@ -11,10 +11,10 @@ function md5(str) {
 
 function relPath(base, filePath) {
 	if (filePath.indexOf(base) !== 0) {
-		return filePath;
+		return filePath.replace(/\\/g, '/');
 	}
-	var newPath = filePath.substr(base.length);
-	if (newPath[0] === path.sep) {
+	var newPath = filePath.substr(base.length).replace(/\\/g, '/');
+	if (newPath[0] === '/') {
 		return newPath.substr(1);
 	} else {
 		return newPath;
