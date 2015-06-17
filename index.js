5832b01579db0f496f4e8c192b8199a9694c8f07
Boris Ceranic <me@sosko.in.rs>
2015-06-17 08:51:39 +0200

    Fix indentation

--- index.js
+++ index.js
@@ -127,10 +127,7 @@ plugin.manifest = function (pth, opts) {
 		firstFileBase = firstFileBase || file.base;
 
 		var revisionedFile = relPath(firstFileBase, file.path);
-		var originalFile = path.join(
-			path.dirname(revisionedFile),
-			path.basename(file.revOrigPath)
-		);
+		var originalFile = path.join(path.dirname(revisionedFile), path.basename(file.revOrigPath));
 
 		manifest[originalFile] = revisionedFile;
 
