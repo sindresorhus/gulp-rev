2bb737eb4d4b79e06ddfe9d8404adce5b9b131c6
Gabin <hello@gabinaureche.com>
2017-04-29 16:26:29 +0200

    make tests look synchronous with pEvent

--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 import path from 'path';
 import test from 'ava';
+import pEvent from 'p-event';
 import gutil from 'gulp-util';
 import rev from './';
 
@@ -18,166 +19,138 @@ function createFile({path, revOrigPath, revOrigBase, origName, revName, cwd, bas
 	return file;
 }
 
-test.cb('should rev files', t => {
+test('should rev files', async t => {
 	const stream = rev();
-
-	stream.on('data', file => {
-		t.is(file.path, 'unicorn-d41d8cd98f.css');
-		t.is(file.revOrigPath, 'unicorn.css');
-		t.end();
-	});
+	const data = pEvent(stream, 'data');
 
 	stream.write(createFile({
 		path: 'unicorn.css'
 	}));
+	stream.end();
+
+	const file = await data;
+	t.is(file.path, 'unicorn-d41d8cd98f.css');
+	t.is(file.revOrigPath, 'unicorn.css');
 });
 
-test.cb('should add the revision hash before the first `.` in the filename', t => {
+test('should add the revision hash before the first `.` in the filename', async t => {
 	const stream = rev();
-
-	stream.on('data', file => {
-		t.is(file.path, 'unicorn-d41d8cd98f.css.map');
-		t.is(file.revOrigPath, 'unicorn.css.map');
-		t.end();
-	});
+	const data = pEvent(stream, 'data');
 
 	stream.write(createFile({
 		path: 'unicorn.css.map'
 	}));
-
 	stream.end();
+
+	const file = await data;
+	t.is(file.path, 'unicorn-d41d8cd98f.css.map');
+	t.is(file.revOrigPath, 'unicorn.css.map');
 });
 
-test.cb('should build a rev manifest file', t => {
+test('should build a rev manifest file', async t => {
 	const stream = rev.manifest();
+	const data = pEvent(stream, 'data');
 
-	stream.on('data', newFile => {
-		t.is(newFile.relative, 'rev-manifest.json');
-		t.deepEqual(
-			JSON.parse(newFile.contents.toString()),
-			{'unicorn.css': 'unicorn-d41d8cd98f.css'}
-		);
-		t.end();
-	});
-
-	const file = createFile({
+	stream.write(createFile({
 		path: 'unicorn-d41d8cd98f.css',
 		revOrigPath: 'unicorn.css'
-	});
-
-	stream.write(file);
+	}));
 	stream.end();
+
+	const file = await data;
+	t.is(file.relative, 'rev-manifest.json');
+	t.deepEqual(
+		JSON.parse(file.contents.toString()),
+		{'unicorn.css': 'unicorn-d41d8cd98f.css'}
+	);
 });
 
-test.cb('should allow naming the manifest file', t => {
+test('should allow naming the manifest file', async t => {
 	const path = 'manifest.json';
 	const stream = rev.manifest({path});
+	const data = pEvent(stream, 'data');
 
-	stream.on('data', newFile => {
-		t.is(newFile.relative, path);
-		t.end();
-	});
-
-	const file = createFile({
+	stream.write(createFile({
 		path: 'unicorn-d41d8cd98f.css',
 		revOrigPath: 'unicorn.css'
-	});
-
-	stream.write(file);
+	}));
 	stream.end();
+
+	const file = await data;
+	t.is(file.relative, path);
 });
 
-test.cb('should append to an existing rev manifest file', t => {
+test('should append to an existing rev manifest file', async t => {
 	const stream = rev.manifest({
 		path: 'test.manifest-fixture.json',
 		merge: true
 	});
+	const data = pEvent(stream, 'data');
 
-	stream.on('data', newFile => {
-		t.is(newFile.relative, 'test.manifest-fixture.json');
-		t.deepEqual(
-			JSON.parse(newFile.contents.toString()),
-			{
-				'app.js': 'app-a41d8cd1.js',
-				'unicorn.css': 'unicorn-d41d8cd98f.css'
-			}
-		);
-		t.end();
-	});
-
-	const file = createFile({
+	stream.write(createFile({
 		path: 'unicorn-d41d8cd98f.css',
 		revOrigPath: 'unicorn.css'
-	});
-
-	stream.write(file);
+	}));
 	stream.end();
+
+	const file = await data;
+	t.is(file.relative, 'test.manifest-fixture.json');
+	t.deepEqual(
+		JSON.parse(file.contents.toString()),
+		{
+			'app.js': 'app-a41d8cd1.js',
+			'unicorn.css': 'unicorn-d41d8cd98f.css'
+		}
+	);
 });
 
-test.cb('should not append to an existing rev manifest by default', t => {
+test('should not append to an existing rev manifest by default', async t => {
 	const stream = rev.manifest({path: 'test.manifest-fixture.json'});
+	const data = pEvent(stream, 'data');
 
-	stream.on('data', newFile => {
-		t.is(newFile.relative, 'test.manifest-fixture.json');
-		t.deepEqual(
-			JSON.parse(newFile.contents.toString()),
-			{'unicorn.css': 'unicorn-d41d8cd98f.css'}
-		);
-		t.end();
-	});
-
-	const file = createFile({
+	stream.write(createFile({
 		path: 'unicorn-d41d8cd98f.css',
 		revOrigPath: 'unicorn.css'
-	});
-
-	stream.write(file);
+	}));
 	stream.end();
+
+	const file = await data;
+	t.is(file.relative, 'test.manifest-fixture.json');
+	t.deepEqual(
+		JSON.parse(file.contents.toString()),
+		{'unicorn.css': 'unicorn-d41d8cd98f.css'}
+	);
 });
 
-test.cb('should sort the rev manifest keys', t => {
+test('should sort the rev manifest keys', async t => {
 	const stream = rev.manifest({
 		path: 'test.manifest-fixture.json',
 		merge: true
 	});
+	const data = pEvent(stream, 'data');
 
-	stream.on('data', newFile => {
-		t.deepEqual(
-			Object.keys(JSON.parse(newFile.contents.toString())),
-			['app.js', 'pony.css', 'unicorn.css']
-		);
-		t.end();
-	});
-
-	const file = createFile({
+	stream.write(createFile({
 		path: 'unicorn-d41d8cd98f.css',
 		revOrigPath: 'unicorn.css'
-	});
-
-	const fileTwo = createFile({
+	}));
+	stream.write(createFile({
 		path: 'pony-d41d8cd98f.css',
 		revOrigPath: 'pony.css'
-	});
-
-	stream.write(file);
-	stream.write(fileTwo);
+	}));
 	stream.end();
+
+	const file = await data;
+	t.deepEqual(
+		Object.keys(JSON.parse(file.contents.toString())),
+		['app.js', 'pony.css', 'unicorn.css']
+	);
 });
 
-test.cb('should respect directories', t => {
+test('should respect directories', async t => {
 	const stream = rev.manifest();
+	const data = pEvent(stream, 'data');
 
-	stream.on('data', newFile => {
-		const MANIFEST = {};
-		MANIFEST[path.join('foo', 'unicorn.css')] = path.join('foo', 'unicorn-d41d8cd98f.css');
-		MANIFEST[path.join('bar', 'pony.css')] = path.join('bar', 'pony-d41d8cd98f.css');
-
-		t.is(newFile.relative, 'rev-manifest.json');
-		t.deepEqual(JSON.parse(newFile.contents.toString()), MANIFEST);
-		t.end();
-	});
-
-	const file1 = createFile({
+	stream.write(createFile({
 		cwd: __dirname,
 		base: __dirname,
 		path: path.join(__dirname, 'foo', 'unicorn-d41d8cd98f.css'),
@@ -185,9 +158,8 @@ test.cb('should respect directories', t => {
 		revOrigBase: __dirname,
 		origName: 'unicorn.css',
 		revName: 'unicorn-d41d8cd98f.css'
-	});
-
-	const file2 = createFile({
+	}));
+	stream.write(createFile({
 		cwd: __dirname,
 		base: __dirname,
 		path: path.join(__dirname, 'bar', 'pony-d41d8cd98f.css'),
@@ -195,27 +167,23 @@ test.cb('should respect directories', t => {
 		revOrigPath: path.join(__dirname, 'bar', 'pony.css'),
 		origName: 'pony.css',
 		revName: 'pony-d41d8cd98f.css'
-	});
-
-	stream.write(file1);
-	stream.write(file2);
+	}));
 	stream.end();
+
+	const MANIFEST = {};
+	MANIFEST[path.join('foo', 'unicorn.css')] = path.join('foo', 'unicorn-d41d8cd98f.css');
+	MANIFEST[path.join('bar', 'pony.css')] = path.join('bar', 'pony-d41d8cd98f.css');
+
+	const file = await data;
+	t.is(file.relative, 'rev-manifest.json');
+	t.deepEqual(JSON.parse(file.contents.toString()), MANIFEST);
 });
 
-test.cb('should respect files coming from directories with different bases', t => {
+test('should respect files coming from directories with different bases', async t => {
 	const stream = rev.manifest();
+	const data = pEvent(stream, 'data');
 
-	stream.on('data', newFile => {
-		const MANIFEST = {};
-		MANIFEST[path.join('foo', 'scriptfoo.js')] = path.join('foo', 'scriptfoo-d41d8cd98f.js');
-		MANIFEST[path.join('bar', 'scriptbar.js')] = path.join('bar', 'scriptbar-d41d8cd98f.js');
-
-		t.is(newFile.relative, 'rev-manifest.json');
-		t.deepEqual(JSON.parse(newFile.contents.toString()), MANIFEST);
-		t.end();
-	});
-
-	const file1 = createFile({
+	stream.write(createFile({
 		cwd: __dirname,
 		base: path.join(__dirname, 'output'),
 		path: path.join(__dirname, 'output', 'foo', 'scriptfoo-d41d8cd98f.js'),
@@ -224,9 +192,8 @@ test.cb('should respect files coming from directories with different bases', t =
 		revOrigPath: path.join(__dirname, 'vendor1', 'foo', 'scriptfoo.js'),
 		origName: 'scriptfoo.js',
 		revName: 'scriptfoo-d41d8cd98f.js'
-	});
-
-	const file2 = createFile({
+	}));
+	stream.write(createFile({
 		cwd: __dirname,
 		base: path.join(__dirname, 'output'),
 		path: path.join(__dirname, 'output', 'bar', 'scriptbar-d41d8cd98f.js'),
@@ -234,26 +201,30 @@ test.cb('should respect files coming from directories with different bases', t =
 		revOrigPath: path.join(__dirname, 'vendor2', 'bar', 'scriptbar.js'),
 		origName: 'scriptfoo.js',
 		revName: 'scriptfoo-d41d8cd98f.js'
-	});
-
-	stream.write(file1);
-	stream.write(file2);
+	}));
 	stream.end();
+
+	const MANIFEST = {};
+	MANIFEST[path.join('foo', 'scriptfoo.js')] = path.join('foo', 'scriptfoo-d41d8cd98f.js');
+	MANIFEST[path.join('bar', 'scriptbar.js')] = path.join('bar', 'scriptbar-d41d8cd98f.js');
+
+	const file = await data;
+	t.is(file.relative, 'rev-manifest.json');
+	t.deepEqual(JSON.parse(file.contents.toString()), MANIFEST);
 });
 
-test.cb('should store the hashes for later', t => {
+test('should store the hashes for later', async t => {
 	const stream = rev();
-
-	stream.on('data', file => {
-		t.is(file.path, 'unicorn-d41d8cd98f.css');
-		t.is(file.revOrigPath, 'unicorn.css');
-		t.is(file.revHash, 'd41d8cd98f');
-		t.end();
-	});
+	const data = pEvent(stream, 'data');
 
 	stream.write(createFile({
 		path: 'unicorn.css'
 	}));
+
+	const file = await data;
+	t.is(file.path, 'unicorn-d41d8cd98f.css');
+	t.is(file.revOrigPath, 'unicorn.css');
+	t.is(file.revHash, 'd41d8cd98f');
 });
 
 test.cb('should handle sourcemaps transparently', t => {
@@ -316,47 +287,41 @@ test.cb('should be okay when the optional sourcemap.file is not defined', t => {
 	}));
 });
 
-test.cb('should handle a . in the folder name', t => {
+test('should handle a . in the folder name', async t => {
 	const stream = rev();
-
-	stream.on('data', file => {
-		t.is(file.path, 'mysite.io/unicorn-d41d8cd98f.css');
-		t.is(file.revOrigPath, 'mysite.io/unicorn.css');
-		t.end();
-	});
+	const data = pEvent(stream, 'data');
 
 	stream.write(createFile({
 		path: 'mysite.io/unicorn.css'
 	}));
+
+	const file = await data;
+	t.is(file.path, 'mysite.io/unicorn-d41d8cd98f.css');
+	t.is(file.revOrigPath, 'mysite.io/unicorn.css');
 });
 
-test.cb('should use correct base path for each file', t => {
+test('should use correct base path for each file', async t => {
 	const stream = rev.manifest();
+	const data = pEvent(stream, 'data');
 
-	stream.on('data', newFile => {
-		const MANIFEST = {};
-		MANIFEST[path.join('foo', 'scriptfoo.js')] = path.join('foo', 'scriptfoo-d41d8cd98f.js');
-		MANIFEST[path.join('bar', 'scriptbar.js')] = path.join('bar', 'scriptbar-d41d8cd98f.js');
-
-		t.deepEqual(JSON.parse(newFile.contents.toString()), MANIFEST);
-		t.end();
-	});
-
-	const fileFoo = createFile({
+	stream.write(createFile({
 		cwd: 'app/',
 		base: 'app/',
 		path: path.join('app', 'foo', 'scriptfoo-d41d8cd98f.js'),
 		revOrigPath: 'scriptfoo.js'
-	});
-
-	const fileBar = createFile({
+	}));
+	stream.write(createFile({
 		cwd: 'assets/',
 		base: 'assets/',
 		path: path.join('assets', 'bar', 'scriptbar-d41d8cd98f.js'),
 		revOrigPath: 'scriptbar.js'
-	});
-
-	stream.write(fileFoo);
-	stream.write(fileBar);
+	}));
 	stream.end();
+
+	const MANIFEST = {};
+	MANIFEST[path.join('foo', 'scriptfoo.js')] = path.join('foo', 'scriptfoo-d41d8cd98f.js');
+	MANIFEST[path.join('bar', 'scriptbar.js')] = path.join('bar', 'scriptbar-d41d8cd98f.js');
+
+	const file = await data;
+	t.deepEqual(JSON.parse(file.contents.toString()), MANIFEST);
 });
