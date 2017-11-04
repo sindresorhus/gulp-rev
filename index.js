cdf63fa1dc2477de03378a2164785967b1ef5013
WVmf <WVmf@users.noreply.github.com>
2017-11-04 12:58:57 +0100

    Pass resolved, absolute paths to relPath() (#220) (#222)
    The original code would fail to determine the correct relative path when
    `file.base` is a relative path and `file.path` is absolute. We can fix
    this by converting both paths to an absolute path (using `path.resolve()`
    and `file.cwd`) before passing them to `relPath()`.
    
    The *uses correct base path for each file* test in **manifest.js** was
    also modified to take this relative-absolute case into consideration.

--- index.js
+++ index.js
@@ -128,7 +128,7 @@ plugin.manifest = (pth, opts) => {
 			return;
 		}
 
-		const revisionedFile = relPath(file.base, file.path);
+		const revisionedFile = relPath(path.resolve(file.cwd, file.base), path.resolve(file.cwd, file.path));
 		const originalFile = path.join(path.dirname(revisionedFile), path.basename(file.revOrigPath)).replace(/\\/g, '/');
 
 		manifest[originalFile] = revisionedFile;
